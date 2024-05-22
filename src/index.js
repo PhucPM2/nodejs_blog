a20b62eef44df74190bd4ecd3d318b576b8534a3
HuynPhuc <phuc.huynhphuc2k2@hcmut.edu.vn>
2024-05-22 09:09:15 +0700

    aaaaaa

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(morgan("combined"));
 app.use(
     express.urlencoded({
-        extended: true,
+         extended: true,
     }),
 );
 app.use(express.json());
